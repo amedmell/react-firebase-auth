0bc5e6bf2c219409cab639bebf4546556b77b1d4
amedmell <amedmellouk@gmail.com>
2022-08-28 15:29:53 +0100

    formik : Signin & Signup

---
+++ src/components/auth/Signin.jsx
@@ -0,0 +1,156 @@
+import React from "react"
+import { useFormik } from "formik"
+import * as Yup from "yup"
+import {Link} from 'react-router-dom'
+import google from "../../assets/images/google.png";
+import facebook from "../../assets/images/facebook.png";
+import github from "../../assets/images/github.png";
+import twitter from "../../assets/images/twitter.png";
+
+export default function Signin() {
+  const formik = useFormik({
+    //inital values
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .required("Must enter username")
+        .matches("[A-Za-z]", "Must only contain letters")
+        .max(15, "too long, use an abreviation please"),
+      password: Yup.string()
+        .required("Must enter a password")
+        .min(8, "At least 8 characters"),
+    }),
+    //submit function that takes form values as args
+    onSubmit: (values) => {
+      alert(JSON.stringify(values));
+    },
+  });
+  return (
+    <>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Login to Your Account
+              </h1>
+              <form
+                onSubmit={formik.handleSubmit}
+                className="space-y-4 md:space-y-6"
+                action="#"
+              >
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="johndoe"
+                    required=""
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  {formik.errors?.username && formik.touched?.username && (
+                    <span className="text-red-500 text-[12px] font-bold">
+                      {formik.errors?.username}
+                    </span>
+                  )}
+                </div>
+             
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  {formik.errors?.password && formik.touched?.password && (
+                    <span className="text-red-500 text-[12px] font-bold">
+                      {formik.errors?.password}
+                    </span>
+                  )}
+                </div>
+              
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                        required=""
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label
+                        htmlFor="remember"
+                        className="text-gray-500 dark:text-gray-300"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Sign in
+                </button>
+              </form>
+              {/* Sign up with socials */}
+              <div className="flex justify-between">
+                <button className="w-14 h-14 bg-slate-100	 rounded-lg">
+                  <img src={google} className='p-2' />
+                </button>
+                <button className="w-14 h-14 bg-slate-100 rounded-lg">
+                  <img src={facebook} className='p-2' />
+                </button>
+                <button className="w-14 h-14 bg-slate-100 rounded-lg">
+                  <img src={github} className='p-2'/>
+                </button>
+                <button className="w-14 h-14 bg-slate-100 rounded-lg">
+                  <img src={twitter} className='p-2'/>
+                </button>
+              </div>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Don't have an account ?{" "}
+                  <Link to={'/signup'} className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                    Sign Up
+                  </Link>
+                </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx,js,tsx,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
