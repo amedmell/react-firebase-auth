f256facf218cc09044dd8b7385b6ad25defda502
amedmell <amedmellouk@gmail.com>
2022-08-29 15:09:28 +0100

    facebook & github auth

--- src/components/auth/Socials.jsx
+++ src/components/auth/Socials.jsx
@@ -2,17 +2,16 @@ import google from "../../assets/images/google.png";
 import facebook from "../../assets/images/facebook.png";
 import github from "../../assets/images/github.png";
 import twitter from "../../assets/images/twitter.png";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth } from "../../firebase/firebase";
 import React,{useContext,useState} from "react";
 import { authContext } from "../Context/AuthContext";
-import { signInWithGoogle } from "./SocialsSignin";
+import { signInWithGoogle ,signInWithFacebook,signInWithGithub} from "./SocialsSignin";
 
 export default function Socials() {
   const [, setCurrentUser] = useContext(authContext);
 
   const googleSignIn=()=>{setCurrentUser(signInWithGoogle())}
-
+  const facebookSignIn=()=>{setCurrentUser(signInWithFacebook())}
+  const githubSignIn=()=>{setCurrentUser(signInWithGithub())}
 
 
   return (
@@ -22,10 +21,10 @@ export default function Socials() {
         <button onClick={googleSignIn} className="w-14 h-14 bg-slate-100	 rounded-lg">
           <img src={google} className="p-2" />
         </button>
-        <button className="w-14 h-14 bg-slate-100 rounded-lg">
+        <button onClick={facebookSignIn} className="w-14 h-14 bg-slate-100 rounded-lg">
           <img src={facebook} className="p-2" />
         </button>
-        <button className="w-14 h-14 bg-slate-100 rounded-lg">
+        <button onClick={githubSignIn} className="w-14 h-14 bg-slate-100 rounded-lg">
           <img src={github} className="p-2" />
         </button>
         <button className="w-14 h-14 bg-slate-100 rounded-lg">
--- src/components/auth/SocialsSignin.js
+++ src/components/auth/SocialsSignin.js
@@ -1,6 +1,12 @@
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 
+//google
 export function signInWithGoogle() {
   let currentUser = null;
   const provider = new GoogleAuthProvider();
@@ -15,5 +21,40 @@ export function signInWithGoogle() {
     })
     .catch((error) => console.log(error));
 
+  return currentUser;
+}
+
+export function signInWithFacebook() {
+  let currentUser = null;
+  const provider = new FacebookAuthProvider();
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user;
+      currentUser = result.user;
+
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+    })
+    .catch((error) => console.log(error));
+  return currentUser;
+}
+
+export function signInWithGithub() {
+  let currentUser = null;
+  const provider = new GithubAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+      currentUser = result.user;
+    })
+    .catch((error) => console.log(error));
   return currentUser;
 }
