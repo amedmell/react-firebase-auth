dc04995ba3d2a4b6e891178c0f5edba4ce1a4694
amedmell <amedmellouk@gmail.com>
2022-08-30 18:42:37 +0100

    CRUD - Add User Profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { authContext } from "./Context/AuthContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 import { useNavigate } from "react-router-dom";
+import Profile from "./User/Profile";
 
 export default function Home() {
   const [currentUser, setCurrentUser] = useContext(authContext);
@@ -21,13 +22,16 @@ export default function Home() {
 
   return (
     <>
-      Welcome {currentUser?.email}
-      <button
-        onClick={SignOut}
-        className="text-white rounded-lg bg-blue-600 w-24 h-12"
-      >
-        Log Out
-      </button>
+      <div className="container">
+        <button
+          onClick={SignOut}
+          className="text-white rounded-lg bg-blue-600 w-24 h-12"
+        >
+          Log Out
+        </button>
+        <br />
+        <Profile uid={currentUser?.uid} email={currentUser?.email} />
+      </div>
     </>
   );
 }
---
+++ src/components/User/Profile.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+
+export default function Profile(props) {
+  const INPUT_STYLING =
+    "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+  const LABEL_STYLING =
+    "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300";
+  const [profile, setProfile] = useState({});
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const idd = e.target.id;
+    setProfile({
+      uid: props.uid,
+      email: props.email,
+      [idd]: value,
+      ...profile,
+    });
+    console.log(profile);
+  };
+
+  const handleProfile = async (e) => {
+    e.preventDefault();
+    // Add a new document with a generated id.
+    await setDoc(doc(db, "users", props.uid), {
+      ...profile,
+      timeStamp: serverTimestamp(),
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleProfile}>
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <label
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              htmlFor="image"
+            >
+              Profile Photo
+            </label>
+            <input className={INPUT_STYLING} id="image" type="file" />
+          </div>
+          <div>
+            <label htmlFor="first_name" className={LABEL_STYLING}>
+              First name
+            </label>
+            <input
+              type="text"
+              id="first_name"
+              className={INPUT_STYLING}
+              placeholder="John"
+              required=""
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="last_name" className={LABEL_STYLING}>
+              Last name
+            </label>
+            <input
+              type="text"
+              id="last_name"
+              className={INPUT_STYLING}
+              placeholder="Doe"
+              required=""
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="company" className={LABEL_STYLING}>
+              Company
+            </label>
+            <input
+              type="text"
+              id="company"
+              className={INPUT_STYLING}
+              placeholder="Flowbite"
+              required=""
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="phone" className={LABEL_STYLING}>
+              Phone number
+            </label>
+            <input
+              type="tel"
+              id="phone"
+              className={INPUT_STYLING}
+              placeholder="123-45-678"
+              required=""
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="website" className={LABEL_STYLING}>
+              Website URL
+            </label>
+            <input
+              type="url"
+              id="website"
+              className={INPUT_STYLING}
+              placeholder="flowbite.com"
+              required=""
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className={LABEL_STYLING}>
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className={INPUT_STYLING}
+              value={props.email}
+              onChange={handleChange}
+              required=""
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Update Profile
+        </button>
+      </form>
+    </>
+  );
+}
